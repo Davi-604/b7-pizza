9759fc85d5baa4120323d78a17a3154eb9cbb154
Davi-604 <davimedeiroscv100@gmail.com>
2026-02-13 20:05:27 -0300

    feature: add the pizza's GET route and it's service

---
+++ app/api/pizzas/route.ts
@@ -0,0 +1,8 @@
+import { getAllProducts } from "@/services/product";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const pizzas = await getAllProducts();
+
+    return NextResponse.json({ pizzas })
+}
