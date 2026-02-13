6a2292bfcd3169a5f3eef0975b11c9cbffb0dc45
Davi-604 <davimedeiroscv100@gmail.com>
2026-02-13 20:06:16 -0300

    fix: adapt the prisma client config for works with PrismaPg lib

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,23 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg"
+import pg from "pg"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const { Pool } = pg
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
