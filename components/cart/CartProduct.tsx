0ce595ce66f7a22c615462d27d6993f7e0af0141
Davi-604 <davimedeiroscv1001@gmail.com>
2026-03-09 11:27:50 -0300

    feature: adds the handler quantity for cart item

--- components/cart/CartProduct.tsx
+++ components/cart/CartProduct.tsx
@@ -4,6 +4,7 @@ import { CartItem } from "@/types/CartItem"
 import Image from "next/image"
 import { Button } from "../ui/button"
 import { useState } from "react"
+import { useCart } from "@/stores/cart"
 
 type Props = {
     data: CartItem
@@ -16,6 +17,32 @@ export const CartProduct = ({ data }: Props) => {
     let product = products.products.find(item => item.id === data.product_id);
 
     if (!product) return null;
+
+    const cart = useCart();
+
+    const handleMinusClick = () => {
+        if (qt - 1 <= 0) {
+            cart.removeItem(data.product_id)
+
+            return;
+        }
+
+        cart.addItem({
+            product_id: data.product_id,
+            quantity: -1
+        })
+
+        setQt(qt - 1);
+    }
+
+    const handlePlusClick = () => {
+        cart.addItem({
+            product_id: data.product_id,
+            quantity: qt
+        });
+
+        setQt(qt + 1);
+    }
 
     return (
         <div className="flex items-center gap-3">
@@ -37,13 +64,13 @@ export const CartProduct = ({ data }: Props) => {
                 </div>
             </div>
             <div className="flex items-center bg-secondary p-2 rounded-md">
-                <Button size="sm" variant='ghost'>
+                <Button size="sm" variant='ghost' onClick={() => handleMinusClick()}>
                     -
                 </Button>
                 <div className="mx-3">
                     {qt}
                 </div>
-                <Button size="sm" variant='ghost'>
+                <Button size="sm" variant='ghost' onClick={() => handlePlusClick()}>
                     +
                 </Button>
             </div>
