875f1c945a7bf922ce14efdeb41e781441204282
Davi-604 <davimedeiroscv1001@gmail.com>
2026-03-19 16:31:49 -0300

    feature: add the price and subtotal on create an order

--- services/order.ts
+++ services/order.ts
@@ -2,21 +2,36 @@ import { prisma } from "@/lib/prisma";
 import { CartItem } from "@/types/CartItem";
 
 export const createNewOrder = async (user_id: number, cart: CartItem[]) => {
+    const order_products = [];
+    let subtotal = 0;
+
+    for (let cart_item of cart) {
+        const product = await prisma.product.findUnique({
+            where: { id: cart_item.product_id }
+        });
+
+        if (product) {
+            order_products.push({
+                product_id: product.id,
+                price: parseFloat(product.price.toString()),
+                quantity: cart_item.quantity
+            });
+
+            subtotal += cart_item.quantity * parseFloat(product.price.toString());
+        }
+    }
+
     const new_order = await prisma.order.create({
         data: {
             user_id,
+            subtotal,
+            orderProducts: {
+                createMany: {
+                    data: order_products
+                }
+            }
         }
     })
-
-    for (let cart_item of cart) {
-        await prisma.orderProducts.create({
-            data: {
-                order_id: new_order.id,
-                product_id: cart_item.product_id,
-                quantity: cart_item.quantity
-            }
-        })
-    }
 
     return new_order;
 }
